b0c631a20fdeb99f90181710654d25ef39e01baa
Elizabeth Harris <ebharris93@gmail.com>
2015-08-28 13:04:54 -0700

    Second commit

--- testsite/wsgi.py
+++ testsite/wsgi.py
@@ -1,30 +1,18 @@
-# """
-# WSGI config for testsite project.
+"""
+WSGI config for testsite project.
 
-# It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-# For more information on this file, see
-# https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-# """
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
 
-# import os
+import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testsite.settings")
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 
- # +++++++++++ DJANGO +++++++++++
-    import os
-    import sys
 
-    ## assuming your Django settings file is at '/home/my_username/projects/my_project/settings.py'
-     path = '/Users/elizabethharris/Desktop/testsite'
-     if path not in sys.path:
-          sys.path.append(path)
-  
-      os.environ['DJANGO_SETTINGS_MODULE'] = 'testsite.settings'
-  
-      from django.core.wsgi import get_wsgi_application
-      application = get_wsgi_application()
